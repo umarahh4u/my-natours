e5739923c0b1e86065a188699c2b5510d1690451
umarahh4u <umarahh4u@gmail.com>
2023-09-25 23:36:28 +0100

    implemented cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,6 +35,18 @@ const limiter = rateLimit({
 });
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, natours.com
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com'
+//   })
+// );
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors())
 
 // Serving static files
 // app.use(express.static(`${__dirname}/public`));
